a8dc31824f0dc0ee6e1b57522f9910cfc08d039b
Kw-jinwoo <woodykwon01@gmail.com>
2024-07-17 13:36:23 +0900

    7/17-init

--- src/main/java/ac/kr/dankook/ace/dom_t1/controller/Dom_Controller.java
+++ src/main/java/ac/kr/dankook/ace/dom_t1/controller/Dom_Controller.java
@@ -1,6 +1,5 @@
 package ac.kr.dankook.ace.dom_t1.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +10,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class Dom_Controller {
 
-    @Autowired
-    private WebConfig webconfig;
 
     @GetMapping("/user/main")
     public String Dom_main(Model model)
